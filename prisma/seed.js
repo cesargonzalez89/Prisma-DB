70e938245442b99a000d059952f96bbae91acfab
Cesar Gonzalez <jugador409@hotmail.com>
2022-05-10 22:20:53 -0500

    Ejecucion de linter

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,100 +1,100 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 (async function main() {
-  try {
-    const woopa = await prisma.explorer.upsert({
-      where: { name: 'Woopa' },
-      update: {},
-      create: {
-        name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
-      },
-    });
+    try {
+        const woopa = await prisma.explorer.upsert({
+            where: { name: "Woopa" },
+            update: {},
+            create: {
+                name: "Woopa",
+                username: "ajolonauta",
+                mission: "Node"
+            },
+        });
 
-    const woopa1 = await prisma.explorer.upsert({
-      where: { name: 'Woopa1' },
-      update: {},
-      create: {
-        name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
-      },
-    });
+        const woopa1 = await prisma.explorer.upsert({
+            where: { name: "Woopa1" },
+            update: {},
+            create: {
+                name: "Woopa1",
+                username: "ajolonauta1",
+                mission: "Node"
+            },
+        });
 
-    const woopa2 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 2' },
-      update: {},
-      create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
-      },
-    });
+        const woopa2 = await prisma.explorer.upsert({
+            where: { name: "Woopa 2" },
+            update: {},
+            create: {
+                name: "Woopa 2",
+                username: "ajolonauta2",
+                mission: "Java"
+            },
+        });
 
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
+        const woopa3 = await prisma.explorer.upsert({
+            where: { name: "Woopa 3" },
+            update: {},
+            create: {
+                name: "Woopa 3",
+                username: "ajolonauta3",
+                mission: "Node"
+            },
+        });
 
-    const mission1 = await prisma.mission.upsert({
-        where: { name: 'node' },
-        update: {},
-        create: {
-            name: 'node',
-            lang: 'node',
-            missionCommander: 'Carlo',
-            enrollments: 900 
-        },
-      });
+        const mission1 = await prisma.mission.upsert({
+            where: { name: "node" },
+            update: {},
+            create: {
+                name: "node",
+                lang: "node",
+                missionCommander: "Carlo",
+                enrollments: 900 
+            },
+        });
   
-      const mission2 = await prisma.mission.upsert({
-        where: { name: 'java' },
-        update: {},
-        create: {
-            name: 'java',
-            lang: 'java',
-            missionCommander: 'Fernanda',
-            enrollments: 400 
-        },
-      });
+        const mission2 = await prisma.mission.upsert({
+            where: { name: "java" },
+            update: {},
+            create: {
+                name: "java",
+                lang: "java",
+                missionCommander: "Fernanda",
+                enrollments: 400 
+            },
+        });
 
-      const missionCommander1 = await prisma.missionCommander.upsert({
-        where: { username: 'carloInovaccion' },
-        update: {},
-        create: {
-            name: 'Carlo',
-            username: 'carloInovaccion',
-            mainStack: 'elixir',
-            currentEnrollment: true,
-            hasAzureCertification: true
-        },
-      });
+        const missionCommander1 = await prisma.missionCommander.upsert({
+            where: { username: "carloInovaccion" },
+            update: {},
+            create: {
+                name: "Carlo",
+                username: "carloInovaccion",
+                mainStack: "elixir",
+                currentEnrollment: true,
+                hasAzureCertification: true
+            },
+        });
 
-      const missionCommander2 = await prisma.missionCommander.upsert({
-        where: { username: 'ferInovaccion' },
-        update: {},
-        create: {
-            name: 'Fernanda',
-            username: 'ferInovaccion',
-            mainStack: 'java',
-            currentEnrollment: false,
-            hasAzureCertification: false
-        },
-      });
+        const missionCommander2 = await prisma.missionCommander.upsert({
+            where: { username: "ferInovaccion" },
+            update: {},
+            create: {
+                name: "Fernanda",
+                username: "ferInovaccion",
+                mainStack: "java",
+                currentEnrollment: false,
+                hasAzureCertification: false
+            },
+        });
 
-    console.log('Create 3 explorers');
-    console.log('Create 2 missions');
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
+        console.log("Create 3 explorers");
+        console.log("Create 2 missions");
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
 })();
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 // Require para usar Prisma
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 //Cors
@@ -11,150 +11,150 @@ const cors = require("cors");
 
 const corsOptions = {
     origin: "http://localhost:8081"
-}
+};
 
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-  res.json({message: 'alive'});
+app.get("/", (req, res) => {
+    res.json({message: "alive"});
 });
 
 //explorers
-app.get('/explorers', async (req, res) => {
+app.get("/explorers", async (req, res) => {
     const allExplorers =  await prisma.explorer.findMany({});
     res.json(allExplorers);
-  });
+});
 
-  app.get('/explorers/:id', async (req, res) => {
+app.get("/explorers/:id", async (req, res) => {
     const id = req.params.id;
     const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
     res.json(explorer);
-  });
-
-  app.post('/explorers', async (req, res) => {
-    const explorer = {
-      name: req.body.name,
-      username: req.body.username,
-      mission: req.body.mission
-     };
-    const message = 'Explorer creado.';
-    await prisma.explorer.create({data: explorer});
-    return res.json({message});
-  });
-
-  app.put('/explorers/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
-
-	await prisma.explorer.update({
-		where: {
-			id: id
-		},
-		data: {
-			mission: req.body.mission
-		}
-	})
-
-	return res.json({message: "Actualizado correctamente"});
 });
 
-app.delete('/explorers/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
-	await prisma.explorer.delete({where: {id: id}});
-	return res.json({message: "Eliminado correctamente"});
+app.post("/explorers", async (req, res) => {
+    const explorer = {
+        name: req.body.name,
+        username: req.body.username,
+        mission: req.body.mission
+    };
+    const message = "Explorer creado.";
+    await prisma.explorer.create({data: explorer});
+    return res.json({message});
+});
+
+app.put("/explorers/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    await prisma.explorer.update({
+        where: {
+            id: id
+        },
+        data: {
+            mission: req.body.mission
+        }
+    });
+
+    return res.json({message: "Actualizado correctamente"});
+});
+
+app.delete("/explorers/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    await prisma.explorer.delete({where: {id: id}});
+    return res.json({message: "Eliminado correctamente"});
 });
 
 //missions
-app.get('/missions', async (req, res) => {
+app.get("/missions", async (req, res) => {
     const allMissions =  await prisma.mission.findMany({});
     res.json(allMissions);
-  });
+});
 
-  app.get('/missions/:id', async (req, res) => {
+app.get("/missions/:id", async (req, res) => {
     const id = req.params.id;
     const mission = await prisma.mission.findUnique({where: {id: parseInt(id)}});
     res.json(mission);
-  });
-
-  app.post('/missions', async (req, res) => {
-    const mission = {
-      name: req.body.name,
-      lang: req.body.lang,
-      missionCommander: req.body.missionCommander,
-      enrollments: req.body.enrollments,
-      hasCertification: req.body.hasCertification
-     };
-    const message = 'Mission creada.';
-    await prisma.mission.create({data: mission});
-    return res.json({message});
-  });
-
-  app.put('/missions/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
-
-	await prisma.mission.update({
-		where: {
-			id: id
-		},
-		data: {
-			enrollments: req.body.enrollments
-		}
-	})
-
-	return res.json({message: "Actualizado correctamente"});
 });
 
-app.delete('/missions/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
-	await prisma.mission.delete({where: {id: id}});
-	return res.json({message: "Eliminado correctamente"});
+app.post("/missions", async (req, res) => {
+    const mission = {
+        name: req.body.name,
+        lang: req.body.lang,
+        missionCommander: req.body.missionCommander,
+        enrollments: req.body.enrollments,
+        hasCertification: req.body.hasCertification
+    };
+    const message = "Mission creada.";
+    await prisma.mission.create({data: mission});
+    return res.json({message});
+});
+
+app.put("/missions/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    await prisma.mission.update({
+        where: {
+            id: id
+        },
+        data: {
+            enrollments: req.body.enrollments
+        }
+    });
+
+    return res.json({message: "Actualizado correctamente"});
+});
+
+app.delete("/missions/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    await prisma.mission.delete({where: {id: id}});
+    return res.json({message: "Eliminado correctamente"});
 });
 
 //missionComander
-app.get('/missionCommander', async (req, res) => {
-  const allMissionComander =  await prisma.missionCommander.findMany({});
-  res.json(allMissionComander);
+app.get("/missionCommander", async (req, res) => {
+    const allMissionComander =  await prisma.missionCommander.findMany({});
+    res.json(allMissionComander);
 });
 
-app.get('/missionCommander/:id', async (req, res) => {
-  const id = req.params.id;
-  const missionComander = await prisma.missionCommander.findUnique({where: {id: parseInt(id)}});
-  res.json(missionComander);
+app.get("/missionCommander/:id", async (req, res) => {
+    const id = req.params.id;
+    const missionComander = await prisma.missionCommander.findUnique({where: {id: parseInt(id)}});
+    res.json(missionComander);
 });
 
-app.post('/missionCommander', async (req, res) => {
-  const missionCommander = {
-    name: req.body.name,
-    username: req.body.username,
-    mainStack: req.body.mainStack,
-    currentEnrollment: req.body.currentEnrollment,
-    hasAzureCertification: req.body.hasAzureCertification
-   };
-  const message = 'Mission Comander creado.';
-  await prisma.missionCommander.create({data: missionCommander});
-  return res.json({message});
+app.post("/missionCommander", async (req, res) => {
+    const missionCommander = {
+        name: req.body.name,
+        username: req.body.username,
+        mainStack: req.body.mainStack,
+        currentEnrollment: req.body.currentEnrollment,
+        hasAzureCertification: req.body.hasAzureCertification
+    };
+    const message = "Mission Comander creado.";
+    await prisma.missionCommander.create({data: missionCommander});
+    return res.json({message});
 });
 
-app.put('/missionCommander/:id', async (req, res) => {
-const id = parseInt(req.params.id);
+app.put("/missionCommander/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
 
-await prisma.missionCommander.update({
-  where: {
-    id: id
-  },
-  data: {
-    mainStack: req.body.mainStack
-  }
-})
+    await prisma.missionCommander.update({
+        where: {
+            id: id
+        },
+        data: {
+            mainStack: req.body.mainStack
+        }
+    });
 
-return res.json({message: "Actualizado correctamente"});
+    return res.json({message: "Actualizado correctamente"});
 });
 
-app.delete('/missionCommander/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-  await prisma.missionCommander.delete({where: {id: id}});
-  return res.json({message: "Eliminado correctamente"});
+app.delete("/missionCommander/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    await prisma.missionCommander.delete({where: {id: id}});
+    return res.json({message: "Eliminado correctamente"});
 });
 
 app.listen(port, () => {
-  console.log(`Listening to requests on port ${port}`);
+    console.log(`Listening to requests on port ${port}`);
 });
